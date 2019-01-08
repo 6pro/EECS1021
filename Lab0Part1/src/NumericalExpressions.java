e002c96fd39ac5ef5a9044d316a01dc2e02a1d79
prithkeswani <pkeswani@my.yorku.ca>
2019-01-08 13:15:53 -0500

    Transfered Lab0 Finsihed files

---
+++ Lab0Part1/src/NumericalExpressions.java
@@ -0,0 +1,14 @@
+
+public class NumericalExpressions {
+
+	public static void main(String[] args) {
+	//comment not executed by computer
+	System.out.println(15+4);
+	System.out.println(15-4);
+	System.out.println(15*4);
+	System.out.println(15/4);
+	System.out.println(23/4);
+	System.out.println(23%4);
+	System.out.println("1+1");
+	}
+}
