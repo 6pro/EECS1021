8943d754d5631c2a2e50833287b845c177a9d15e
prithkeswani <pkeswani@my.yorku.ca>
2019-01-12 22:38:03 -0500

    Lab0Part2 Upploaded
    BMI CALCULATOR NOT COMPLETE

---
+++ Lab0Part2/src/ComputeAndInterpretBMI.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class ComputeAndInterpretBMI {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner (System.in);
+		System.out.println("Lab Test 1: "+ labTest1 );
+
+		System.out.println("Lab Test 1: "+ labTest1 );
+
+		System.out.println("Lab Test 1: "+ labTest1 );
+
+		System.out.println("Lab Test 1: "+ labTest1 );
+
+	}
+
+}
