ad74e06a7c21684a5b80fdd8f3a599b70011574f
prithkeswani <pkeswani@my.yorku.ca>
2019-01-15 12:36:01 -0500

    Update ComputeAndInterpretBMI.java
    Added finishing touches to the code

--- Lab0Part2/src/ComputeAndInterpretBMI.java
+++ Lab0Part2/src/ComputeAndInterpretBMI.java
@@ -1,16 +1,65 @@
-import java.util.*;
+
+//import scanner
+import java.util.Scanner;
+
+//import Math class
+import java.lang.Math;
+
 public class ComputeAndInterpretBMI {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner input = new Scanner (System.in);
-		System.out.println("Lab Test 1: "+ labTest1 );
 
-		System.out.println("Lab Test 1: "+ labTest1 );
+		// create scanner
+		Scanner input = new Scanner(System.in);
 
-		System.out.println("Lab Test 1: "+ labTest1 );
+		// declare variables
+		double weight;
+		int feet;
+		int inches;
 
-		System.out.println("Lab Test 1: "+ labTest1 );
+		// prompt user
+		System.out.print("Enter weight in pounds: ");
+		weight = input.nextFloat();
+
+		System.out.print("Enter feet: ");
+		feet = input.nextInt();
+
+		System.out.print ("Enter inches: ");
+		inches = input.nextInt();
+
+		// convert measurements
+		double weightInKilos = weight * 0.453592;
+		double heightInMeters = (((feet * 12) + inches) * .0254);
+		double bmi = weightInKilos / Math.pow(heightInMeters, 2.0);
+		// double bmi = weightInKilos / (heightInMeters * heightInMeters);
+
+		// display output
+		System.out.println("Your BMI is: " + bmi);
+
+		// interpret BMI
+		if (bmi < 18.5) {
+			System.out.print("Underweight");
+		}
+
+		else if (bmi >= 18.5 && bmi < 25) {
+			System.out.print("Normal");
+		}
+
+		else if (bmi >= 25 && bmi < 30) {
+			System.out.print("Overweight");
+		}
+
+		else if (bmi >= 30) {
+			System.out.print("Obese");
+		}
+
+//      Do I need this last else if there?
+//      else {
+//          System.out.print("");
+//      }
+
+		input.close();
 
 	}
 
